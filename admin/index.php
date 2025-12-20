f8b8a6836b01b3bfa7fc9a1fa1134082bfc94986
reubenjanuardi <reubenjanuardi@gmail.com>
2025-12-20 20:44:20 +0700

    Add .htaccess and index.php files for admin routing

---
+++ admin/index.php
@@ -0,0 +1,13 @@
+<?php
+
+define('LARAVEL_START', microtime(true));
+
+require __DIR__ . '/../backend/vendor/autoload.php';
+$app = require_once __DIR__ . '/../backend/bootstrap/app.php';
+
+$kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
+$response = $kernel->handle(
+    $request = Illuminate\Http\Request::capture()
+)->send();
+
+$kernel->terminate($request, $response);
